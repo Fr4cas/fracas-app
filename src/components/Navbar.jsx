f34e39a8ef668a2b51acb59e6dbe3453676563ed
Lucas Lin <chenghsin2007@gmail.com>
2025-08-26 16:53:48 +0800

    style: fix navbar position to top

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,13 @@
+import React from 'react'
+import '../styles/Navbar.css'
+
 function Navbar(){
     return(
-        <>
+        <nav className="navbar">
             <ul>
                 <li>this will be the navbar</li>
             </ul>
-        </>
+        </nav>
     )
 }
 
