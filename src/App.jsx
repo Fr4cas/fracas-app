68ff69996c11ced1fdc20df339d5637c3669acb9
Lucas Lin <chenghsin2007@gmail.com>
2025-08-26 16:15:24 +0800

    feat: add simple Navbar component to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './css/App.css'
 import Hero from './components/Hero'
+import Navbar from './components/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Navbar />
       <Hero />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+function Navbar(){
+    return(
+        <>
+            <ul>
+                <li>this will be the navbar</li>
+            </ul>
+        </>
+    )
+}
+
+export default Navbar
