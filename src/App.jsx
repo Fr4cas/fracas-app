7202ee3175decd92693182fcbc4af4d594abda7e
Lucas Lin <chenghsin2007@gmail.com>
2025-08-26 15:57:55 +0800

    chore: restructure and clean folder layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,16 @@
 import { useState } from 'react'
-import './App.css'
+import './css/App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Vite + React</h1>
+      <h1>TEMP</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
     </>
   )
