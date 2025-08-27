4e5e33d806a2a335867ad804b51e4c4d78fc3601
Lucas Lin <chenghsin2007@gmail.com>
2025-08-27 08:59:27 +0800

    feat: add GitHub link with logo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,14 @@
 import '../styles/Footer.css'
+import githubLogo from "../assets/logos/github-white.svg"
 
 function Footer() {
     return (
         <>
-        <footer className="footer">
-            <ul>
-                <li>This will be the footer</li>
-            </ul>
-        </footer>
-            
+            <footer className="footer">
+                <ul>
+                    <li><a href="https://github.com/Fr4cas" target='_blank' rel='noopener noreferrer'><img src={githubLogo} alt="GitHub" className='logo'/></a></li>
+                </ul>
+            </footer>
         </>
     )
 }
