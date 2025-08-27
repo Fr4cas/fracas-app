8e060ea9cef49a5cb5b164bcf6baf575c90257ec
Lucas Lin <chenghsin2007@gmail.com>
2025-08-27 08:29:08 +0800

    style: fix footer position to bottom

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,14 @@
+import '../styles/Footer.css'
+
 function Footer() {
     return (
         <>
+        <footer className="footer">
             <ul>
                 <li>This will be the footer</li>
             </ul>
+        </footer>
+            
         </>
     )
 }
