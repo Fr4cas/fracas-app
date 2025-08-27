433d20476c83dd5e18070c28eb0aba6c651258f4
Lucas Lin <chenghsin2007@gmail.com>
2025-08-27 09:51:28 +0800

    feat: add description field and placehold p tag

--- src/components/Hobby.jsx
+++ src/components/Hobby.jsx
@@ -1,5 +1,11 @@
 function Hobby() {
-    const hobbies = [{ title: "Gaming" }, { title: "Music" }, { title: "Coding" }];
+    const hobbies = [
+        // TODO: add descriptions
+        { title: "Gaming", description: "" }, 
+        { title: "Music", description: "" }, 
+        { title: "Coding", description: "" }, 
+        { title: "Japanese", description: "" }
+    ];
 
     return (
         <>
@@ -7,6 +13,7 @@ function Hobby() {
                 {hobbies.map((hobby, index) => (
                     <div className="hobby-card" key={index}>
                         <h3>{hobby.title}</h3>
+                        <p>{hobby.description}</p>
                     </div>
                 ))}
             </div>
