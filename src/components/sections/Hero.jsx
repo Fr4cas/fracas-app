e438b70aad58803af09ce1d47ba3e9c832461b3b
Lucas Lin <chenghsin2007@gmail.com>
2025-08-29 08:42:08 +0800

    feat: restyle hero section and add image

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -1,12 +1,20 @@
 import '../../styles/sections/Hero.css'
+import heroCover from "../../assets/img/hero-cover.jpg";
 
 function Hero() {
     return (
         <>
             <section>
-                <div className="hero__content">
-                    <h1>Fracas</h1>
-                    <h2>Still in the process of learning...</h2>
+                <div className="hero">
+                    <div className="hero__content">
+                        <div className="hero__text">
+                            <h1>Fracas</h1>
+                            <h2>Still in the process of learning...</h2>
+                        </div>
+                        <div className="hero__content-image">
+                            <img src={heroCover} alt="hero cover image" />
+                        </div>
+                    </div>
                 </div>
             </section>
         </>
